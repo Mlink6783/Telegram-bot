6ece242df9d455b77442f519326f4b2ec8dbb167
Mlink6783 <mlink6783@gmail.com>
2025-04-23 20:46:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -154,3 +154,7 @@ async def telegram_webhook(request: Request):
 @app.api_route("/", methods=["GET", "HEAD"])
 async def root(request: Request):
     return JSONResponse(content={"status": "Bot is running"})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
